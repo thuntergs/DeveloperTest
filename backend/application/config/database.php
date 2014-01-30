25f9b5f672024b8a88302a8ee1ba66955f707b1c
Brian Hosie <brianhosie@gmail.com>
2014-01-30 13:43:14 -0700

    rm db pw

--- backend/application/config/database.php
+++ backend/application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'siPFrIwvthWgu29tbLOoWOf67';
+$db['default']['password'] = '';
 $db['default']['database'] = 'vet';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
