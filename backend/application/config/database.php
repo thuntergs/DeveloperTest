820d5be10ef0eefe0ab5387edfc96671bdcebdb4
Brian Hosie <brianhosie@gmail.com>
2014-01-30 13:41:52 -0700

    updated image file perms

--- backend/application/config/database.php
+++ backend/application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'siPFrIwvthWgu29tbLOoWOf67';
 $db['default']['database'] = 'vet';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
